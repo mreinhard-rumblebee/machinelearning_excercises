ce33812d7bcb3644d9c68e2baa3f88f605220095
jochenmadler <58168497+jochenmadler@users.noreply.github.com>
2022-06-23 15:36:39 +0200

    added implementation for value- and policy update and loss function.

--- PPO_template/main_heiko.py
+++ PPO_template/main_heiko.py
@@ -1,7 +1,7 @@
-
 from keras.models import Sequential, Model
 from keras.layers import Dense, Input
 import tensorflow as tf
+from keras import backend as K
 import gym
 import numpy as np
 import scipy.signal
@@ -17,104 +17,107 @@ class GymEnvironment:
     def trainPPO(self, agent, no_episodes):
         self.runPPO(agent, no_episodes, training=True)
 
-
     def runPPO(self, agent, no_episodes, training=False):
 
         rew = np.zeros(no_episodes)
         for episode in range(no_episodes):
             state = self.env.reset().reshape(1, self.env.observation_space.shape[0])
-            advantages = []
-            gradients = []
-            states = []
+            states, actions, G_lams, values_global, logprobs = [], [], [], [], []
 
-            for n in range(0,self.actors):
+            for n in range(0, self.actors):
                 state = self.env.reset().reshape(1, self.env.observation_space.shape[0])
-                #Problem: if done==1, the pole will always fall, so a reset might be good ???
+                # Problem: if done==1, the pole will always fall, so a reset might be good ???
                 tot_rew = 0
                 values, rewards = [], []
                 for t in range(self.max_timesteps):
                     # TODO: Fill out the respective to-dos in this loop and make sure that the overall algorithm works,
                     #  e.g., overwrite current state with next state entering a new time step
 
-                    logit,action = agent.select_action(state)
-
+                    logit, action = agent.select_action(state)
                     next_state, reward, done, _ = self.env.step(action)
                     next_state = next_state.reshape(1, self.env.observation_space.shape[0])
                     tot_rew += reward
 
                     states.append(state)
-
+                    actions.append(action)
 
                     if training == True:
+                        # TODO: Store relevant transition information such as rewards, values, etc. that you will need in
+                        #  the calculation of the advantages later
                         rewards.append(reward)
                         values.append(agent.critic.predict(state)[0])
-                    # TODO: Store relevant transition information such as rewards, values, etc. that you will need in
-                    #  the calculation of the advantages later
+                        logprobs.append(policy_probabilities(logit, action))
 
                     state = next_state
 
-                    if (done==True or t==self.max_timesteps) and training == True:
+                    if (done == True or t == self.max_timesteps) and training == True:
                         # TODO: Call function for calculation and storage of advantages
-                        #Calculate advantages when the function breaks or the last iteration is reached
-                        advantages.append(agent.calc_advantage(values, rewards, 0, t))
+                        # Calculate advantages when the function breaks or the last iteration is reached
+                        G_lams.extend(agent.calc_advantage(values, rewards, 0, t))
+                        values_global.extend(values)
+
                         gradients.append()
-                        #TODO: Store targets for your value function update
+                        # TODO: Store targets for your value function update
+
                         break
                 rew.append(tot_rew)
 
-            #TODO: If training, call function to update policy function weights using clipping
-            #TODO: If training, Call function to update value function weights
+            # TODO: If training, call function to update policy function weights using clipping
+            # TODO: If training, Call function to update value function weights
+
             # TODO: Implement here a function that evaulates the agent's performance for every x episodes by -- NOT YET
             # calling runDQN directly and returns an average of total rewards for 100 runs, if your objective is
             # reached, you can terminate training
         return rew
 
 
-def policy_probabilities(logit):
-    probs = np.exp(logit) / np.sum(np.exp(logit))
-    return probs
-        # TODO: Compute probabilities of taking actions a by using the outputs of actor NN (the logits)
+def policy_probabilities(logit, action):
+    # TODO: Compute probabilities of taking actions a by using the outputs of actor NN (the logits)
+    # softmax calculation
+    logprobs = tf.math.log(np.exp(logit) / np.sum(np.exp(logit)))
+    logprob = tf.reduce_sum(tf.one_hot(action, 2) * logprobs, axis=1)
+    return logprob
+
 
 # Sum of discountated rewards of vectors --> useful for advantage estimates or total rewards
 def discounted_cumulative_sums(x, discount):
-        return scipy.signal.lfilter([1], [1, float(-discount)], x[::-1], axis=0)[::-1]
+    return scipy.signal.lfilter([1], [1, float(-discount)], x[::-1], axis=0)[::-1]
+
 
 class PPO_Agent:
     def __init__(self, no_of_states, no_of_actions):
         self.state_size = no_of_states
         self.action_size = no_of_actions
 
-        #TODO: Set hyperparameters and vary them
+        # TODO: Set hyperparameters and vary them
         self.gamma = 0.9  # discount rate
-        self.lam = 0.6 # lambda for TD(lambda)
-        self.clip_ratio = 0.5 # Clipping ratio for calculating L_clip
-        self.actors = 100 # Number of parallel actors
+        self.lam = 0.6  # lambda for TD(lambda)
+        self.clip_ratio = 0.5  # Clipping ratio for calculating L_clip
+        self.actors = 100  # Number of parallel actors
 
-        self.actor = self.nn_model(self.state_size,self.action_size)
-        self.critic =self.nn_model(self.state_size,1)
+        self.actor = self.nn_model(self.state_size, self.action_size)
+        self.critic = self.nn_model(self.state_size, 1)
 
-
-    def select_action(self,state):
+    def select_action(self, state):
+        # TODO: Implement action selection, i.e., sample an action from policy pi
         logit = self.actor.predict(state)[0]
-        probs = policy_probabilities(logit)
-        action = np.random.choice([0,1], p=probs)
+        action = tf.squeeze(tf.random.categorical(logit, 1), axis=1)
         return logit, action
-        #TODO: Implement action selection, i.e., sample an action from policy pi
 
-    def calc_advantage(self,values,rew,n,T):
-        #Using offline forward-looking TD(lambda) with one update per episode
-        TD = []
+    def calc_advantage(self, values, rew, n, T):
+        # Using offline forward-looking TD(lambda) with one update per episode
+        G_lam = []
         for t in range(T):
             acc_rew = 0
             rew_list = []
             for k in range(T - t):
-                acc_rew = acc_rew + self.gamma**k * rew[t + k]
-                V_tmp = self.gamma**(k + 1) * values[t + k + 1] if k < T - t - 1 else 0
-                rew_list.append((acc_rew + V_tmp) * self.lam**k)
+                acc_rew = acc_rew + self.gamma ** k * rew[t + k]
+                V_tmp = self.gamma ** (k + 1) * values[t + k + 1] if k < T - t - 1 else 0
+                rew_list.append((acc_rew + V_tmp) * self.lam ** k)
 
             G_t = (1 - self.lam) * np.sum(rew_list[:-1]) + rew_list[-1]
 
-            TD.append(G_t - values[t])
+            G_lam.append(G_t)
 
             """
             G_n = np.zeros(T-t+1)
@@ -131,38 +134,48 @@ class PPO_Agent:
             G_lam = (1-self.lam) * G_disc + G_n[T-t] * self.lam**(T-t-1)
             """
 
-        #Consider normalizing the advantages:
-        #TD = (TD - np.mean(TD)) / (np.std(TD) + 1e-10)
+            return G_lam
 
-        return TD
-        #TODO: Implement here the calculation of the advantage, e.g., using TD-lambda or eligibility traces
+        # Consider normalizing the advantages:
+        # TD = (TD - np.mean(TD)) / (np.std(TD) + 1e-10)
 
-    def nn_model(self,state_size,output_size):
+        # TODO: Implement here the calculation of the advantage, e.g., using TD-lambda or eligibility traces
+
+    def nn_model(self, state_size, output_size):
         layer_input = Input(state_size)
-        layer_one = Dense(512, input_shape=state_size, activation="relu", kernel_initializer='random_normal',bias_initializer='zeros')(layer_input)
-        layer_two = Dense(512, activation="relu", kernel_initializer='random_normal', bias_initializer='zeros')(layer_one)
-        layer_three = Dense(512, activation="relu", kernel_initializer='random_normal', bias_initializer='zeros')(layer_two)
-        layer_four = Dense(output_size, activation="linear", kernel_initializer='random_normal',bias_initializer='zeros')(layer_three)
+        layer_one = Dense(512, input_shape=state_size, activation="relu", kernel_initializer='random_normal',
+                          bias_initializer='zeros')(layer_input)
+        layer_two = Dense(512, activation="relu", kernel_initializer='random_normal', bias_initializer='zeros')(
+            layer_one)
+        layer_three = Dense(512, activation="relu", kernel_initializer='random_normal', bias_initializer='zeros')(
+            layer_two)
+        layer_four = Dense(output_size, activation="linear", kernel_initializer='random_normal',
+                           bias_initializer='zeros')(layer_three)
 
         model = Model(inputs=layer_input, outputs=layer_four)
         model.compile(loss="mse", metrics=["accuracy"])
         print(model)
-        #TODO: Define the neural network here, make sure that you account for the different requirements of the value
+        # TODO: Define the neural network here, make sure that you account for the different requirements of the value
         # function and policy function approximation in the in- and outputs
 
     # Here newly observed transitions are stored in the experience replay buffer
-    def record(self): # TODO: add the relevant input arguments that you will need to store
-        # TODO: Define here arrays in which you will store all the information that you need in the advantage
-        #  calculation, e.g., rewards, values, states, etc.
+    def record(self):  # TODO: add the relevant input arguments that you will need to store
 
-    @tf.function # This is a wrapper that when adding it in front of a function, consisting only of tf syntax,
+    # TODO: Define here arrays in which you will store all the information that you need in the advantage
+    #  calculation, e.g., rewards, values, states, etc.
+
+    @tf.function  # This is a wrapper that when adding it in front of a function, consisting only of tf syntax,
     # can improve speed
-    def update_policy_parameters(self):
+    def update_policy_parameters(self, states, actions, logprobs, G_lams, values_global):
 
         with tf.GradientTape() as tape:  # Record operations for automatic differentiation.
             # TODO: Use the advantages and calculated policies to calculated the clipping function here and calculate
             #  the loss function
-            pol_loss =
+            ratio = tf.exp(policy_probabilities(self.actor.predict(states)[0], actions) - logprobs)
+
+            advantages = list(map(lambda x, y: x - y, G_lams, values_global))
+            clip = K.clip(ratio, min_value=1 - self.clip_ratio, max_value=1 + self.clip_ratio) * advantages
+            pol_loss = -K.mean(K.minimum(ratio * advantages, clip))
 
         pol_grads = tape.gradient(pol_loss, self.actor.trainable_variables)
         self.actor.apply_gradients(zip(pol_grads, self.actor.trainable_variables))
@@ -170,12 +183,13 @@ class PPO_Agent:
     # This is a wrapper that when adding it in front of a function, consisting only of tf syntax,
     # can improve speed
     @tf.function
-    def update_value_parameters(self):
+    def update_value_parameters(self, alpha, G_lams, values_global):
 
         with tf.GradientTape() as tape:  # Record operations for automatic differentiation.
             # TODO: Use the advantages and calculated policies to calculated the clipping function here and calculate
             #  the loss function
-            val_loss = ...
+            val_loss = alpha * tf.keras.metrics.mean_squared_error(G_lams, values_global)
+
         val_grads = tape.gradient(val_loss, self.critic.trainable_variables)
         self.critic.apply_gradients(zip(val_grads, self.critic.trainable_variables))
 
@@ -183,29 +197,27 @@ class PPO_Agent:
 if __name__ == "__main__":
     environment = GymEnvironment('CartPole-v0', 'gymresults/cartpole-v0')
 
-
-    no_of_states = 4 #TODO: Define number of states # [position of cart, velocity of cart, angle of pole, rotation rate of pole]
-    no_of_actions = 2 #TODO: Define number of actions # [left, right]
+    no_of_states = 4  # TODO: Define number of states # [position of cart, velocity of cart, angle of pole, rotation rate of pole]
+    no_of_actions = 2  # TODO: Define number of actions # [left, right]
 
     # The agent is initialized
-    agent = PPO_Agent(no_of_states,no_of_actions)
+    agent = PPO_Agent(no_of_states, no_of_actions)
 
     # Train your agent
-    no_episodes = 500 # TODO: Play around with this number
+    no_episodes = 500  # TODO: Play around with this number
     environment.trainPPO(agent, no_episodes)
 
     # Run your agent
     no_episodes_run = 100
-    agent.actors = 1 # This is set to one here as multiple actors are only required for training
+    agent.actors = 1  # This is set to one here as multiple actors are only required for training
     rew = environment.runPPO(agent, no_episodes_run)
 
     # TODO: Implement here a function visualizing/plotting, e.g., -- NOT YET
     # your agent's performance over the number of training episodes
 
-
-#Useful Links:
-#https://www.youtube.com/watch?v=lYP3cF2wqOY
-#https://pylessons.com/PPO-reinforcement-learning
-#https://github.com/pythonlessons/Reinforcement_Learning/blob/master/11_Pong-v0_PPO/Pong-v0_PPO_TF2.py
-#https://towardsdatascience.com/understanding-and-implementing-proximal-policy-optimization-schulman-et-al-2017-9523078521ce
-#https://medium.com/intro-to-artificial-intelligence/the-actor-critic-reinforcement-learning-algorithm-c8095a655c14
+# Useful Links:
+# https://www.youtube.com/watch?v=lYP3cF2wqOY
+# https://pylessons.com/PPO-reinforcement-learning
+# https://github.com/pythonlessons/Reinforcement_Learning/blob/master/11_Pong-v0_PPO/Pong-v0_PPO_TF2.py
+# https://towardsdatascience.com/understanding-and-implementing-proximal-policy-optimization-schulman-et-al-2017-9523078521ce
+# https://medium.com/intro-to-artificial-intelligence/the-actor-critic-reinforcement-learning-algorithm-c8095a655c14
