5adb9d5459cdb0fecf4de5b63ffe7a0a298656aa
rumblebee13 <65600684+rumblebee13@users.noreply.github.com>
2022-07-25 01:24:47 +0200

    reworked comments & readjusted environment to v0

--- DQN_template/main.py
+++ DQN_template/main.py
@@ -8,20 +8,18 @@ import numpy as np
 import random
 
 import matplotlib.pyplot as plt
-from numpy.core._simd import targets
 
 
 class GymEnvironment:
     def __init__(self, env_id, monitor_dir, max_timesteps=100000):
         self.max_timesteps = max_timesteps
-
         self.env = gym.make(env_id)
 
-    def trainDQN(self, agent, no_episodes, visualize_agent=False ):
-        rew = self.runDQN(agent, no_episodes, training=True, visualize_agent=visualize_agent )
+    def trainDQN(self, agent, no_episodes, visualize_agent=False):
+        rew = self.runDQN(agent, no_episodes, training=True, visualize_agent=visualize_agent)
 
         # Automatically save weights of trained network
-        agent.model.save_weights("cartpole-v1.h5", overwrite=True)
+        agent.model.save_weights("cartpole-v0.h5", overwrite=True)
         return rew
 
     def runDQN(self, agent, no_episodes, training=False, visualize_agent=False, evaluation=False):
@@ -33,15 +31,16 @@ class GymEnvironment:
             i = 0
             rwd = 0
             while not done:
+                #possibility to visualize training or testing
                 if visualize_agent:
                     self.env.render()
                 action = agent.select_action(state)
                 next_state, reward, done, _ = self.env.step(action)
-                next_state = np.reshape(next_state, [1,  agent.state_size])
+                next_state = np.reshape(next_state, [1, agent.state_size])
                 rwd += reward
+                # introduce negative rewards to incentivice agent to finish
                 if not done or i == self.max_timesteps - 1:
                     reward = reward
-
                 else:
                     reward = -100
                 if training:
@@ -50,25 +49,23 @@ class GymEnvironment:
                 i += 1
                 if training:
                     agent.update_weights(i)
-
                 if done:
                     break
-
             rew[episode] = rwd
 
             # TODO: Implement here a function that evaulates the agent's performance for every x episodes by
             # calling runDQN directly and returns an average of total rewards for 100 runs, if your objective is
             # reached, you can terminate training
-
-            if episode%50 == 0 and training:
+            if episode % 50 == 0 and training:
                 print("-------------------------------------------")
                 print("Evaluation starts:")
                 rew_subset = self.runDQN(agent, 100, training=False, evaluation=True)
                 avg_subset = sum(rew_subset) / 100
                 print("Average over 100 episodes: {}".format(
                     avg_subset))
-                if avg_subset > 200:
+                if avg_subset >= 200:
                     break
+            # if evaluating, just plot the progress
             if not evaluation:
                 if training:
                     print("episode: {}/{} | score: {} | e: {:.3f}".format(
@@ -77,7 +74,7 @@ class GymEnvironment:
                     print("episode: {}/{} | score: {}".format(
                         episode + 1, no_episodes, rwd))
             else:
-                if episode%10==0:
+                if episode % 10 == 0:
                     print("Progress: {} %".format(episode))
         return rew
 
@@ -92,9 +89,8 @@ class DQN_Agent:
         self.gamma = 0.9  # discount rate
         self.epsilon = 1.0  # eps-greedy exploration rate
         self.batch_size = 64  # maximum size of the batches sampled from memory
-        self.epsilon_min = 0.005
+        self.epsilon_min = 0.001
         self.epsilon_decay = 0.99
-
 
         # TODO: Initialize the neural network models of weight and target weight networks
         self.model = self.nn_model((no_of_states,), no_of_actions, load_old_model)
@@ -140,21 +136,16 @@ class DQN_Agent:
         if self.epsilon > self.epsilon_min:
             self.epsilon *= self.epsilon_decay
 
-    # TODO Rework this function!
-    def update_weights(self, t):  # t for time stepp at wich target moddle will be updated
+    def update_weights(self, t):  # t for time step at wich target model will be updated
         # TODO: Define the function to update weights of your network
         if len(self.memory) < self.batch_size:
             return
         # Randomly sample minibatch from the memory
         minibatch = random.sample(self.memory, min(self.batch_size, self.batch_size))
-
         state = np.zeros((self.batch_size, self.state_size))
         next_state = np.zeros((self.batch_size, self.state_size))
         action, reward, done = [], [], []
 
-        # do this before prediction
-        # for speedup, this could be done on the tensor level
-        # but easier to understand using a loop
         for i in range(self.batch_size):
             state[i] = minibatch[i][0]
             action.append(minibatch[i][1])
@@ -162,22 +153,20 @@ class DQN_Agent:
             next_state[i] = minibatch[i][3]
             done.append(minibatch[i][4])
 
-        # do batch prediction to save speed
+        # predict the whole batch
         target = self.model.predict(state)
         target_next = self.model.predict(next_state)
         target_val = self.target_model.predict(next_state)
 
         for i in range(self.batch_size):
-            # correction on the Q value for the action used
             if done[i]:
                 target[i][action[i]] = reward[i]
             else:
                 a = np.argmax(target_next[i])
                 # target Q Network evaluates the action
-                # Q_max = Q_target(s', a'_max)
                 target[i][action[i]] = reward[i] + self.gamma * (target_val[i][a])
 
-        # Train the Neural Network with batches
+        # fit model
         self.model.fit(state, target, batch_size=self.batch_size, verbose=0)
 
         if (t % self.target_model_time) == 0:
@@ -185,7 +174,7 @@ class DQN_Agent:
 
 
 if __name__ == "__main__":
-    environment = GymEnvironment('CartPole-v1', 'gymresults/cartpole-v1')
+    environment = GymEnvironment('CartPole-v0', 'gymresults/cartpole-v0')
 
     # TODO: Define the number of states and actions - you will need this information for defining your neural network
     no_of_states = 4
@@ -198,20 +187,24 @@ if __name__ == "__main__":
     agent = DQN_Agent(no_of_states, no_of_actions, load_old_model)
 
     # Train your agent
-    no_episodes = 5
-    visualize_agent = False
-    rew_train = environment.trainDQN(agent, no_episodes, visualize_agent)
+    no_episodes = 10000
+    visualize_agent_train = False
+    rew_train = environment.trainDQN(agent, no_episodes, visualize_agent_train)
 
     # Run your agent
-    no_episodes_run = 5
-    visualize_agent = False
-    rew_test = environment.runDQN(agent, no_episodes_run, visualize_agent)
+    no_episodes_run = 100
+    visualize_agent_test = False
+    rew_test = environment.runDQN(agent, no_episodes_run, visualize_agent_test)
 
     # TODO: Implement here a function visualizing/plotting, e.g.,
     print("-------------------------------------------")
-    print("Average Score: ".format(sum(rew_test)/100))
+    print("Average Score: {}".format(sum(rew_test) / 100))
+    print("-------------------------------------------")
+    print("Training Rewards: {}".format(rew_train))
+    print("-------------------------------------------")
+    print("Testing Rewards: {}".format(rew_test))
+    print("-------------------------------------------")
     plotting = True
-    print(rew_train, rew_test)
     if plotting:
         fig, (ax1, ax2) = plt.subplots(1, 2)
         ax1.plot(rew_train)
@@ -224,37 +217,21 @@ if __name__ == "__main__":
         plt.show()
         plt.savefig('Plots.png', dpi=600)
 
-
-        # plt.plot(rew_test)
-        # plt.xlabel("Episode")
-        # plt.ylabel("Score")
-        # plt.title('Testing')
-        # plt.savefig('Testing.png', dpi=300)
-        # plt.show()
-
-
-    # your agent's performance over the number of training episodes
-    #     iterations = range(0, no_episodes + 1, no_episodes_run)
-    #     plt.plot(iterations, rew)
-    #     plt.ylabel('Average Return')
-    #     plt.xlabel('Iterations')
-    #     plt.ylim(top=250)
-
-
         # Here you can watch a simulation on how your agent performs after being trained.
         # NOTE that this part will try to load an existing set of weights, therefore set visualize_agent to TRUE, when you
-    #     # already saved a set of weights from a training session
-    # if visualize_agent_ == True:
-    #     env = gym.make('CartPole-v0')
-    #     load_model = 1
-    #     state_size = 4
-    #     action_size = 2
-    #     agent = DQN_Agent(state_size, action_size, load_model)
-    #     state = env.reset().reshape(1, env.observation_space.shape[0])
-    #     for _ in range(1000):
-    #         env.render()
-    #         action = agent.select_action(state, training=False)
-    #         next_state, reward, done, _ = env.step(action)
-    #         next_state = next_state.reshape(1, env.observation_space.shape[0])
-    #         state = next_state
-    #     env.close()
+        # already saved a set of weights from a training session
+    visualize_agent_afterwards = False
+    if visualize_agent_afterwards == True:
+        env = gym.make('CartPole-v0')
+        load_model = 1
+        state_size = 4
+        action_size = 2
+        agent = DQN_Agent(state_size, action_size, load_model)
+        state = env.reset().reshape(1, env.observation_space.shape[0])
+        for _ in range(1000):
+            env.render()
+            action = agent.select_action(state, training=False)
+            next_state, reward, done, _ = env.step(action)
+            next_state = next_state.reshape(1, env.observation_space.shape[0])
+            state = next_state
+        env.close()
